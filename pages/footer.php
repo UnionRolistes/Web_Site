d7f03f7efaec91b8704ce3478206da87ae46c24d
Youssef <agoussalyo@eisti.eu>
2021-07-29 19:52:09 +0200

    Changed footer + better responsive

--- pages/footer.php
+++ pages/footer.php
@@ -3,7 +3,7 @@
         <h5><a rel="license" href="http://creativecommons.org/licenses/by/2.0/fr/">
             <img alt="Licence Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by/2.0/fr/80x15.png"/>
         </a> 
-        &copy; 2021 Page créée par l'Union Des Rôlistes. Association et marque deposée. Version BETA</h5>
+        &copy; 2021 Page créée par <a href="http://unionrolistes.fr/">l'Union Des Rôlistes</a>. Association et marque deposée. Version BETA</h5>
         Ce(tte) œuvre est mise à disposition selon les termes de la <a rel="license" href="http://creativecommons.org/licenses/by/2.0/fr/">Licence Creative Commons Attribution 2.0 France</a>
     </div>
 </footer>
